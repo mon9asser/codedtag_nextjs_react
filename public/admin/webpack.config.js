a2408a7d07e64af9c325cd95c903e482641ddeef
Montasser <nexycode@gmail.com>
2024-06-23 14:04:13 +0300

    updates

---
+++ public/admin/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // other configurations...
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          use: ['source-map-loader'],
+          exclude: /node_modules/
+        },
+      ],
+    },
+    // other configurations...
+};
+  
