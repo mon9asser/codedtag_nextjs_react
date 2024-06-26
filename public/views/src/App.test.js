94fc61fd345dbafa83ac1274c2cd1c6344d2317b
Montasser <nexycode@gmail.com>
2024-06-27 01:14:48 +0300

    updates

--- public/views/src/App.test.js
+++ public/views/src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  console.log("Test App");
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
