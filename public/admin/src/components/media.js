2cb96e85e403255b3f294910434f43f800f41bd3
Montasser <nexycode@gmail.com>
2024-08-01 14:44:17 +0300

    updates

--- public/admin/src/components/media.js
+++ public/admin/src/components/media.js
@@ -9,14 +9,18 @@ var  MediaUploader = () => {
     var toggleModal = () => {
         setOpenUploadModal(! uploadModal);
     }
-
-    
-
+ 
     var ModalOfUploadImage = ({uploadModal, toggleModal, imageData}) => {
+
         var uploadImageElement = React.useRef('');
-        var triggerFileUploader = () => {
+        const [selectedFile, setSelectedFile] = React.useState(null);
+
+
+        var triggerFileUploader = (e) => {
+            e.preventDefault();
             uploadImageElement.current.click();
         }
+
         var setImageObjectCallback = (data) => {
             var main_object = {...imageObject}; 
             var keys = Object.keys(data);
@@ -34,33 +38,60 @@ var  MediaUploader = () => {
             id: ''
         });
 
+        var uploadMediaToServer = (e) => {
+            e.preventDefault(); 
+        }
+
+        var handleFileChange = (event) => {
+            
+            const file = event.target.files[0];
+            if (file) {
+                console.log(file);
+                setSelectedFile(file);
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                    setImageObjectCallback({
+                        url: reader.result
+                    })
+                };
+                reader.readAsDataURL(file);
+            }
+
+        }
+
        return ( 
             <div className={`modal ${uploadModal ? "open_this_modal" : ""}`}>
                 <div className="modal-background" onClick={toggleModal}></div>
-                <div className="modal-card">
+                <form className="modal-card modal-card-update-css">
                     <header className="modal-card-head">
                         <p className="modal-card-title">{imageObject.name == '' ? "Upload Image": imageObject.name}</p>
                     </header>
-                    <section className="modal-card-body">
-                        <input style={{display: 'none'}} type="file" ref={uploadImageElement} />
-
-                        {imageObject.url == ''?
-                        <button onClick={triggerFileUploader} className="uploadImageBox">
-                            <span>Upload Image</span>
-                        </button>
-                        : "Image url is found show it"}
-                        <input placeholder="Title ( Image alt attribute )" value={imageObject.title} onChange={e => setImageObjectCallback({title: e.target.value})} type="text" />
-                        <input placeholder="Name" value={imageObject.name} onChange={e => setImageObjectCallback({name: e.target.value})} type="text" />
-                        <textarea placeholder="Description" value={imageObject.description} onChange={e => setImageObjectCallback({description: e.target.value})} />
+                    <section className="modal-card-body modal-card-body-update-css">
+                        <div>
+                            <input style={{display: 'none'}} type="file" onChange={handleFileChange} accept="image/*" ref={uploadImageElement} />
+                            {imageObject.url == ''?
+                            <button onClick={triggerFileUploader} className="uploadImageBox">
+                                <span>Upload Image</span>
+                            </button>
+                            : <img src={imageObject.url} alt={imageObject.title} className="image-responsive" />}
+                        </div>
+                        <div>
+                            <input placeholder="url ( Full Image URL )" value={imageObject.url} onChange={e => setImageObjectCallback({url: e.target.value})} type="text" readOnly={true} />
+                            <input placeholder="Title ( Image alt attribute )" value={imageObject.title} onChange={e => setImageObjectCallback({title: e.target.value})} type="text" />
+                            <input placeholder="Name" value={imageObject.name} onChange={e => setImageObjectCallback({name: e.target.value})} type="text" />
+                            <textarea placeholder="Description" value={imageObject.description} onChange={e => setImageObjectCallback({description: e.target.value})} />
+                        </div>
                     </section>
                     <footer className="modal-card-foot">
                     <button onClick={toggleModal} className="button">Close</button>
                     <div style={{marginLeft:"auto"}}>
                         <button onClick={toggleModal} className="button red">Delete</button>
-                        <button onClick={toggleModal} className="button blue">Save Media</button>
+                        {
+                            imageObject.id == ''? <button onClick={uploadMediaToServer} type="submit" className="button blue">Save Media</button>: ''
+                        } 
                     </div>
                     </footer>
-                </div>
+                </form>
             </div>
         )
     }
