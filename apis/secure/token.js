8d7ddd3b7fb32c892e3d1b516ddd177b2b6205ab
Your Name <you@example.com>
2024-08-04 20:11:01 +0300

    updates

--- apis/secure/token.js
+++ apis/secure/token.js
@@ -9,21 +9,21 @@ const jwt = require('jsonwebtoken');
 tokenRouter.get("/hash-request", async(req, res) => {
 
     if( ! req.headers.agent || ! req.headers.api_keys ) {
- 
+        console.log('tract 1')
         return res.send({
             is_error: true, 
             message: 'Invalid credentials',
-            data: "trace 1"
+            data: []
         })
     }
 
     // validate api keys 
     if( req.headers.api_keys !== Config.api_keys ) {
-        console.log("trace 2")
+        console.log('tract 2')
         return res.send({
             is_error: true, 
             message: 'Invalid credentials',
-            data: "trace 2"
+            data: []
         })
     }
 
@@ -51,12 +51,11 @@ tokenRouter.get("/hash-request", async(req, res) => {
         });
 
     } catch (error) {
-       
-
+        console.log('tract 3')
         return res.send({
             is_error: true, 
             message: 'Invalid credentials',
-            data: "trace 3"
+            data: []
         })
     }
 
