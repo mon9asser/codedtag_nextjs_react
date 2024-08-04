de484ea3dbfd286443e07f752e37fdb746ad0688
Your Name <you@example.com>
2024-08-04 22:00:30 +0300

    updates

--- public/views/src/settings.js
+++ public/views/src/settings.js
@@ -6,7 +6,7 @@ const Settings = {
 
     google: {
         captcha: {
-            public: '6LefOUopAAAAAERp42vXnixQ6WAUcq7VAnyHjwlt',
+            public: '6LeAXx8qAAAAAFtzvk8IgDbElk5sNro17fLbmlXm',
             secret:'6LefOUopAAAAAKDP80U5BQ8v-U10T_H2IF0WCZ9P'
         }
     },
