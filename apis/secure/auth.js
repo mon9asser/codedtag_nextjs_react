cb2a8cbc2e744292760fb2d258f038a7656b2cc1
montasser <montasser@gmail.com>
2024-08-21 18:25:16 +0300

    updates

--- apis/secure/auth.js
+++ apis/secure/auth.js
@@ -36,9 +36,9 @@ const verifyToken = (req, res, next) => {
   }
   
   try {
-	  
+	 console.log('Config.jwt_secret ::', Config.jwt_secret);
     jwt.verify(token, Config.jwt_secret, (err, decoded) => {
-	console.log("decoded", decoded)
+	
 	console.log("----sitename_dsco", decoded.agent, decoded.site_name)
 	
       if (err || decoded.agent == undefined || decoded.site_name == undefined ) { 
