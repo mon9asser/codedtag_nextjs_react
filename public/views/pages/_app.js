f45a866ea23b620eeafbde45d2778a71ee542796
Montasser <nexycode@gmail.com>
2024-09-14 01:27:53 +0300

    updates

--- public/views/pages/_app.js
+++ public/views/pages/_app.js
@@ -39,8 +39,9 @@ export default function MyApp({ Component, pageProps }) {
   
   var settings = (pageProps.upcoming == undefined || pageProps == undefined) ? null: pageProps.upcoming.settings; 
  
+  if( settings != null )
   console.log(settings.google_ads.enabled);
-  
+
   return (
     <div className={poppins.className}>
         <Component {...pageProps} />
