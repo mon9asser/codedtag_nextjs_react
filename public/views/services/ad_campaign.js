285b26054dbc1c9200f6d362cd29a403fce63ca3
Montasser <nexycode@gmail.com>
2024-09-14 01:35:54 +0300

    updates

--- public/views/services/ad_campaign.js
+++ public/views/services/ad_campaign.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
  
 export default function AdCompaignBox({ position, data, classes, settings }) {
-  
+    /*
     useEffect(() => {
 
       setTimeout(() => {
@@ -11,7 +11,7 @@ export default function AdCompaignBox({ position, data, classes, settings }) {
         }
       }, 100)
     }, []);
-  
+    */
   
     if (!data || !data.length) {
       return null;
